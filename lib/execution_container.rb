4a54842db63478c45e39c10a449aad4d9403a18a
naosuke <naosuke@live.jp>
2017-07-21 19:21:33 +0900

    実行済みコンテナは殺すよう修正

--- lib/execution_container.rb
+++ lib/execution_container.rb
@@ -27,7 +27,7 @@ class ExecutionContainer
     @container.start
     r = @container.exec(['bash','-c', "cd /tmp && #{compile_cmd} #{@time}.#{@lang} && ./#{@time} < #{@time}.in"])
     @container.stop
-    #@container.delete(force: true)
+    @container.delete(force: true)
     return r
   end
 end
